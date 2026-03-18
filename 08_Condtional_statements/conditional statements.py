15a9cc5697ce73ee7f37f62205fd88c1fec15356
VikasGowda M <vikasmanjunath2005@gmail.com>
2026-03-18 10:51:41 +0530

    edited thr format

--- 08_Condtional_statements/conditional statements.py
+++ 08_Condtional_statements/conditional statements.py
@@ -1,54 +1,55 @@
-# #Conditional statements:
-# #donditional statements are used to perform different actions based on different conditions. 
+#Conditional statements:
+#donditional statements are used to perform different actions based on different conditions. 
 
-# #1.if statement: It is used to execute a block of code if a specified condition is true.
-# age = int(input("Enter your age: "))
-# if age >= 18:
-#     print("You are an adult.")
+#1.if statement: It is used to execute a block of code if a specified condition is true.
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
 
-# #2.if-else statement: It is used to execute a block of code if a specified condition is true, and another block of code if the condition is false.
-# age = int(input("Enter your age: "))
-# if age >= 18:
-#     print("You are an adult.")
-# else:
-#     print("You are a minor.")
-
-
-# #3.if-elif-else statement: It is used to execute a block of code if a specified condition is true, and another block of code if the condition is false, and another block of code if the condition is false.
-# age = int(input("Enter your age: "))
-# if age >= 18:
-#     print("You are an adult.")
-# elif age >= 13:
-#     print("You are a teenager.")
-# else:
-#     print("You are a child.")
+#2.if-else statement: It is used to execute a block of code if a specified condition is true, and another block of code if the condition is false.
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
 
 
-
-# #nested if statement: It is used to execute a block of code if a specified condition is true, and another block of code if the condition is false, and another block of code if the condition is true.
-# age = int(input("Enter your age: "))
-# if age >= 18:
-#     print("You are an adult.")
-#     if age >= 65:
-#         print("You are a senior citizen.")
-# elif age >= 13:
-#     print("You are a teenager.")
-# else :
-#     print("You are a child.")
+#3.if-elif-else statement: It is used to execute a block of code if a specified condition is true, and another block of code if the condition is false, and another block of code if the condition is false.
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+elif age >= 13:
+    print("You are a teenager.")
+else:
+    print("You are a child.")
 
 
 
-# #logical operators in conditional statements:
-# age = int(input("Enter your age: "))
-# is_student = input("Are you a student? (yes/no): ")
-# if age >= 18 and is_student.lower() == "yes":
-#     print("You are an adult student.")
-# elif age >= 18 and is_student.lower() == "no":
-#     print("You are an adult non-student.")
-# elif age < 18 and is_student.lower() == "yes":
-#     print("You are a minor student.")
-# else:
-#     print("You are a minor non-student.")
+#nested if statement: It is used to execute a block of code if a specified condition is true, and another block of code if the condition is false, and another block of code if the condition is true.
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+    if age >= 65:
+        print("You are a senior citizen.")
+elif age >= 13:
+    print("You are a teenager.")
+else :
+    print("You are a child.")
+
+
+
+#logical operators in conditional statements:
+age = int(input("Enter your age: "))
+is_student = input("Are you a student? (yes/no): ")
+if age >= 18 and is_student.lower() == "yes":
+    print("You are an adult student.")
+elif age >= 18 and is_student.lower() == "no":
+    print("You are an adult non-student.")
+elif age < 18 and is_student.lower() == "yes":
+    print("You are a minor student.")
+else:
+    print("You are a minor non-student.")
+
 
 
 
