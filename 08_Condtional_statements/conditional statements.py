d75cbd5493722f474d49f5faf7a4547da9cb6a88
VikasGowda M <vikasmanjunath2005@gmail.com>
2026-03-18 10:27:46 +0530

    Nxt topic: Conditional statements

--- 08_Condtional_statements/conditional statements.py
+++ 08_Condtional_statements/conditional statements.py
@@ -2,26 +2,26 @@
 #donditional statements are used to perform different actions based on different conditions. 
 
 #1.if statement: It is used to execute a block of code if a specified condition is true.
-# age = int(input("Enter your age: "))
-# if age >= 18:
-#     print("You are an adult.")
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
 
 #2.if-else statement: It is used to execute a block of code if a specified condition is true, and another block of code if the condition is false.
-# age = int(input("Enter your age: "))
-# if age >= 18:
-#     print("You are an adult.")
-# else:
-#     print("You are a minor.")
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
 
 
 #3.if-elif-else statement: It is used to execute a block of code if a specified condition is true, and another block of code if the condition is false, and another block of code if the condition is false.
-# age = int(input("Enter your age: "))
-# if age >= 18:
-#     print("You are an adult.")
-# elif age >= 13:
-#     print("You are a teenager.")
-# else:
-#     print("You are a child.")
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+elif age >= 13:
+    print("You are a teenager.")
+else:
+    print("You are a child.")
 
 
 
