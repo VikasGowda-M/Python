089ae04685a753527989c84c272687b349bc343a
VikasGowda M <vikasmanjunath2005@gmail.com>
2026-03-27 22:14:43 +0530

    added Sets in python

--- 14_Dictionary_sets/sets.py
+++ 14_Dictionary_sets/sets.py
@@ -1,3 +1,6 @@
 #Sets:
 '''it is collection of immutable elements
-it is unodered'''
+-> it is unodered
+-> set can't store dictionary and lists because they are muttable
+-> sets are mutuablle the values are immutable
+-> sets '''
