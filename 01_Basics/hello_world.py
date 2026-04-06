501f3317c06a1b5260732d13d25044f1ba9b7785
VikasGowda M <vikasmanjunath2005@gmail.com>
2026-04-06 21:11:25 +0530

    updated read me file

--- 01_Basics/hello_world.py
+++ 01_Basics/hello_world.py
@@ -1,2 +1,8 @@
+#What is Python:
+# A simple, high-level programming language used for web, apps, AI, automation, etc.
+# Features of Python:
+# Easy to learn, readable syntax, platform-independent, large libraries, supports multiple programming styles.
+# Installation & Setup:
+# Download from Python official website → Install → Run code using IDLE or any IDE (VS Code, PyCharm).
 #"print" is a built-in function in Python that outputs the specified message to the screen. 
 print("Hello World!")
